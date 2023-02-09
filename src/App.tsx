a43d4f2506c0ac32d6d20e429cfb0ca8a368e3a7
gdiaz18 <giadiaz@udel.edu>
2023-02-09 15:14:41 -0500

    hello world commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript; Gia Diaz
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
+               Hello World. Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
         </div>
