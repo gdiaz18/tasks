7514d2edb13a7b3dfb863ce9ca12eeb3aa7c250d
gdiaz18 <giadiaz@udel.edu>
2023-03-13 15:42:14 -0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import img from "./jack.jpeg";
-import { Button, Col, Container, Row } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
@@ -18,30 +14,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript;
             </header>
-<<<<<<< HEAD
-            <h1>Gia Diaz </h1>
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="column1"></div>
-                        Edit <code>src/App.tsx</code> and save.
-                    </Col>
-                    <Col>
-                        <div className="column2"></div>
-                        This page will automatically reload.
-                    </Col>
-                </Row>
-            </Container>
-            <img src={img} alt="My dog Jack" />
-            <ul>
-                <li>My dog Jack</li>
-                <li>He is a Teacup Shitzu</li>
-                <li>He is 12 years old</li>
-            </ul>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -54,7 +26,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
