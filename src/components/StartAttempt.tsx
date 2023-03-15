743841a3d8ec7d3228ba8ea24a73ce67a6f54017
gdiaz18 <giadiaz@udel.edu>
2023-03-15 15:45:31 -0400

    startAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -1,6 +1,41 @@
+import { getByText } from "@testing-library/react";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setValue] = useState<number>(4);
+    const [inProgress, setState] = useState<boolean>(false);
+
+    function startQuiz(): void {
+        setValue(attempts - 1);
+        setState(true);
+    }
+
+    return (
+        <div>
+            <div>Attempts: {attempts}</div>
+            <div>
+                <Button
+                    onClick={startQuiz}
+                    disabled={inProgress || attempts === 0}
+                >
+                    Start Quiz
+                </Button>
+            </div>
+            <div>
+                <Button onClick={() => setState(false)} disabled={!inProgress}>
+                    Stop Quiz
+                </Button>
+            </div>
+            <div>
+                <Button
+                    onClick={() => setValue(attempts + 1)}
+                    disabled={inProgress}
+                >
+                    Mulligan
+                </Button>
+            </div>
+        </div>
+    );
 }
