cd439c1b3b59c9fac960197d19f73c7b0873fd4e
gdiaz18 <giadiaz@udel.edu>
2023-03-15 15:44:42 -0400

    changeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -1,7 +1,34 @@
+import { type } from "os";
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [whatType, setState] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function changeQuestionType(): void {
+        if (whatType === "short_answer_question") {
+            setState("multiple_choice_question");
+        } else {
+            setState("short_answer_question");
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <Button onClick={changeQuestionType}>Change Type</Button>
+            </div>
+            <div>
+                Current Type:{" "}
+                {whatType === "multiple_choice_question" ? (
+                    <span>Multiple Choice</span>
+                ) : (
+                    <span>Short Answer</span>
+                )}
+            </div>
+        </div>
+    );
 }
