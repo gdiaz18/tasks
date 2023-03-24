b6c66f506c1f8e6311699bb5bd1a53022431a7e7
gdiaz18 <giadiaz@udel.edu>
2023-03-23 22:55:36 -0400

    editmode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,4 +1,4 @@
-import { isVisible } from "@testing-library/user-event/dist/utils";
+//import { isVisible } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
