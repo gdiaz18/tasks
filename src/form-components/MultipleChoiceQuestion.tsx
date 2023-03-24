1d5cc85d55eb34e461ffb5b36b39d96b3ec5eb14
gdiaz18 <giadiaz@udel.edu>
2023-03-23 22:52:16 -0400

    MCQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const DEFAULT_OPTION = options[0];
+    const [currChoice, setCurrChoice] = useState<string>(DEFAULT_OPTION);
+
+    function updateCurrChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrChoice(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="userOptions">
+                <Form.Label>What is your choice?</Form.Label>
+                <Form.Select value={currChoice} onChange={updateCurrChoice}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+                {currChoice === expectedAnswer ? "✔️" : "❌"}
+            </Form.Group>
         </div>
     );
 }
